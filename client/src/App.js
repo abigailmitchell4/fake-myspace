b24f124dab5ac4329661c9db3b1d8ff7e3ffb227
Abigail Mitchell <abby.w.mitchell@gmail.com>
2019-12-16 14:28:59 -0700

    login built

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Fragment, } from 'react';
+import FetchUser from './components/FetchUser';
+import Login from './components/Login';
+import Home from './components/Home';
+import NoMatch from './components/NoMatch';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
+import Register from './components/Register';
+import { Switch, Route, } from 'react-router-dom';
+import { Container, } from "semantic-ui-react";
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <FetchUser>
+      <Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Container>
+    </FetchUser>
+  </Fragment>
+)
+
+export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header, Container, } from 'semantic-ui-react';
+
+const Home = () => (
+  <Container>
+    <Header as="h3" textAlign="center">Fake MySpace</Header>
+  </Container>
+)
+
+export default Home;
